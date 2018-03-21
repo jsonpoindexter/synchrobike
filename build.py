6e8668d64ad0f40dad4adf44c1f972ba2b29f639
Jason Poindexter <jason@launchbadge.com>
2018-03-21 16:15:59 -0700

    Cleanup directory.

--- build.py
+++ build.py
@@ -2,15 +2,16 @@ from subprocess import call, check_call
 import serial.tools.list_ports
 
 import os 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "/build"
-
+dir_path = os.path.dirname(os.path.realpath(__file__))
+build_path = dir_path + "/build"
 print(dir_path)
+print(build_path)
 
 # Build sketch
-command = "/Applications/Arduino.app/Contents/Java/arduino-builder -dump-prefs -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + dir_path + " -warnings=none -build-cache " + dir_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose /Users/jp/.ghq/github.com/jsonpoindexter/synchrobike/sync_animations/sync_animations.ino"
+command = "/Applications/Arduino.app/Contents/Java/arduino-builder -dump-prefs -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
 call(command, shell=True) 
 
-command = "/Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + dir_path + " -warnings=none -build-cache " + dir_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose /Users/jp/.ghq/github.com/jsonpoindexter/synchrobike/sync_animations/sync_animations.ino"
+command = "/Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
 check_call(command, shell=True) 
 
 list = serial.tools.list_ports.comports()
@@ -18,6 +19,6 @@ connected = []
 for element in list:
    connected.append(element.device)
    port = element.device
-   command = "/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9/esptool -vv -cd nodemcu -cb 921600 -cp " + port + " -ca 0x00000 -cf " + dir_path + "/sync_animations.ino.bin"
+   command = "/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9/esptool -vv -cd nodemcu -cb 921600 -cp " + port + " -ca 0x00000 -cf " + build_path + "/synchrobike.ino.bin"
    call(command, shell=True) 
 print("Connected COM ports: " + str(connected))
--- synchrobike.ino
+++ synchrobike.ino
@@ -1,78 +1,143 @@
 //************************************************************
-// this is a simple example that uses the easyMesh library
+// this is a simple example that uses the painlessMesh library
 //
-// 1. blinks led once for every node on the mesh
-// 2. blink cycle repeats every BLINK_PERIOD
-// 3. sends a silly message to every node on the mesh at a random time betweew 1 and 5 seconds
-// 4. prints anything it recieves to Serial.print
-// 
+// 1. sends a silly message to every node on the mesh at a random time betweew 1 and 5 seconds
+// 2. prints anything it recieves to Serial.print
+//
 //
 //************************************************************
-#include <easyMesh.h>
-
-// some gpio pin that is connected to an LED... 
-// on my rig, this is 5, change to the right number of your LED.
-#define   LED             5       // GPIO number of connected LED
-
-#define   BLINK_PERIOD    1000000 // microseconds until cycle repeat
-#define   BLINK_DURATION  100000  // microseconds LED is on for
+#include "painlessMesh.h"
 
 #define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneeky"
+#define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
 
-easyMesh  mesh;
+painlessMesh  mesh;
 
-uint32_t sendMessageTime = 0;
+
+#include <FastLED.h>
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define LED_PIN     13
+#define NUM_LEDS    75
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+CRGBPalette16 currentPalette(CRGB::Black);
+CRGBPalette16 targetPalette(OceanColors_p);
+TBlendType    currentBlending;                                // NOBLEND or LINEARBLEND 
+ 
+static int16_t dist;                                          // A random number for our noise generator.
+uint16_t xscale = 30;                                         // Wouldn't recommend changing this on the fly, or the animation will be really blocky.
+uint16_t yscale = 30;                                         // Wouldn't recommend changing this on the fly, or the animation will be really blocky.
+uint8_t maxChanges = 24;                                      // Value for blending between palettes.
+
+uint8_t direction_change; // How many seconds to wait before animation scroll direction to change
+uint8_t pallette_change; // How many seconds to wait before animation scroll direction to change
 
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %d msg=%s\n", from, msg.c_str());
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 }
 
-void newConnectionCallback( bool adopt ) {
-  Serial.printf("startHere: New Connection, adopt=%d\n", adopt);
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
 
+void changedConnectionCallback() {
+    Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
 
 void setup() {
   Serial.begin(115200);
-    
-  pinMode( LED, OUTPUT );
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
-  mesh.setReceiveCallback( &receivedCallback );
-  mesh.setNewConnectionCallback( &newConnectionCallback );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
 
-  randomSeed( analogRead( A0 ) );  
+  set_max_power_in_volts_and_milliamps(5, 500);               // FastLED Power management set at 5V, 500mA.
+
+  randomSeed(mesh.getNodeId());  // This will cause a semi-random pallet to be created on first boot.
+  direction_change = random(5,10); // How many seconds to wait before animation scroll direction to change
+  pallette_change = random(4,8); // How many seconds to wait before animation scroll direction to change
+
+  targetPalette = CRGBPalette16(CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 192, random(128,255)), CHSV(random(0,255), 255, random(128,255)));
+  
+  dist = random16(mesh.getNodeId()); // A semi-random number for our noise generator
+  
 }
 
 void loop() {
   mesh.update();
+  showAnimations();
+}
 
-  // run the blinky
-  bool  onFlag = false;
-  uint32_t cycleTime = mesh.getNodeTime() % BLINK_PERIOD;
-  for ( uint8_t i = 0; i < ( mesh.connectionCount() + 1); i++ ) {
-    uint32_t onTime = BLINK_DURATION * i * 2;    
 
-    if ( cycleTime > onTime && cycleTime < onTime + BLINK_DURATION )
-      onFlag = true;
+uint8_t prev_second_hand = 0;
+uint8_t prev_millisecond_hand = 0;
+uint8_t prev_direction_time = 0;
+
+bool direction = rand() % 2;
+
+void showAnimations(){
+  int current_time = mesh.getNodeTime();
+  uint8_t millisecond_hand = (current_time / 10000 % 60); // enter every 10ms
+  if (prev_millisecond_hand != millisecond_hand) {   
+    uint8_t maxChanges = 24; 
+    nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);   // AWESOME palette blending capability.
+    fillnoise8();                                                               // Update the LED array with noise at the new location
+    prev_millisecond_hand = millisecond_hand;
+    FastLED.show();
   }
-  digitalWrite( LED, onFlag );
 
-  // get next random time for send message
-  if ( sendMessageTime == 0 ) {
-    sendMessageTime = mesh.getNodeTime() + random( 1000000, 5000000 );
+  // chjange direction every random (s);
+  uint8_t direction_time = (current_time / 1000000 % 60) / direction_change;
+  if(prev_direction_time != direction_time) {
+    Serial.printf("directionChange has been reached: %u\n", direction_time);
+    randomSeed(current_time / 1000000);
+    direction = random(0,255) % 2;
+    direction_change = random(5,6);
+    prev_direction_time = direction_time;
   }
-
-  // if the time is ripe, send everyone a message!
-  if ( sendMessageTime != 0 && sendMessageTime < mesh.getNodeTime() ){
-    String msg = "Hello from node ";
-    msg += mesh.getChipId();
-    mesh.sendBroadcast( msg );
-    sendMessageTime = 0;
+  
+  uint8_t second_hand = (current_time / 1000000 % 60) / 6;
+  if (prev_second_hand != second_hand) { 
+    Serial.printf("change color pallets: %u\n", second_hand);
+    randomSeed(current_time / 1000000);
+    targetPalette = CRGBPalette16(CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 192, random(128,255)), CHSV(random(0,255), 255, random(128,255)));
+    pallette_change = random(4,8);
+    prev_second_hand = second_hand;
   }
 }
+ 
+void fillnoise8() {
+  
+  for(int i = 0; i < NUM_LEDS; i++) {                                      // Just ONE loop to fill up the LED array as all of the pixels change.
+    uint8_t index = inoise8(i*xscale, dist+i* yscale) % 255;               // Get a value from the noise function. I'm using both x and y axis.
+    leds[i] = ColorFromPalette(currentPalette, index, 255, LINEARBLEND);   // With that value, look up the 8 bit colour palette value and assign it to the current LED.
+  }
+
+  if (direction){ // Moving along the distance (that random number we started out with). Vary it a bit with a sine wave.
+    dist += beatsin8(10,1,4, mesh.getNodeTime());                        
+  } else {
+    dist -= beatsin8(10,1,4, mesh.getNodeTime()); 
+  }
+ 
+} // fillnoise8()
+

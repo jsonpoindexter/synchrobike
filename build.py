4a8ab514f1b596bbf78838fe3864b9ac7d6795eb
Jason Poindexter <jason@launchbadge.com>
2018-05-17 12:53:19 -0700

    Support different OS directories (Linux/MacOs)

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from subprocess import call, check_call
 import serial.tools.list_ports
+import os, sys
 
-import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 build_path = dir_path + "/build"
 print(dir_path)
@@ -11,20 +11,33 @@ if not os.path.exists(build_path):
     os.makedirs(build_path)
 
 # Build sketch
-command = "/home/jp/arduino/arduino-builder -dump-prefs -logger=machine -hardware /home/jp/arduino/hardware -hardware /home/jp/.arduino15/packages -tools /home/jp/arduino/tools-builder -tools /home/jp/arduino/hardware/tools/avr -tools /home/jp/.arduino15/packages -built-in-libraries /home/jp/arduino/libraries -libraries /home/jp/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/home/jp/.arduino15/packages/esp8266/tools/mkspiffs/0.2.0 -prefs=runtime.tools.esptool.path=/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/home/jp/.arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
-#command = "/Applications/Arduino.app/Contents/Java/arduino-builder -dump-prefs -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
-call(command, shell=True) 
+if os.name == 'Linux':
+    command = "/home/jp/arduino/arduino-builder -dump-prefs -logger=machine -hardware /home/jp/arduino/hardware -hardware /home/jp/.arduino15/packages -tools /home/jp/arduino/tools-builder -tools /home/jp/arduino/hardware/tools/avr -tools /home/jp/.arduino15/packages -built-in-libraries /home/jp/arduino/libraries -libraries /home/jp/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/home/jp/.arduino15/packages/esp8266/tools/mkspiffs/0.2.0 -prefs=runtime.tools.esptool.path=/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/home/jp/.arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
+elif os.name == 'Darwin' or 'posix':
+    command = "/Applications/Arduino.app/Contents/Java/arduino-builder -dump-prefs -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
+else:
+    sys.exit("os " + os.name + " unsupported yet!")
 
-command = "/home/jp/arduino/arduino-builder -compile -logger=machine -hardware /home/jp/arduino/hardware -hardware /home/jp/.arduino15/packages -tools /home/jp/arduino/tools-builder -tools /home/jp/arduino/hardware/tools/avr -tools /home/jp/.arduino15/packages -built-in-libraries /home/jp/arduino/libraries -libraries /home/jp/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/home/jp/.arduino15/packages/esp8266/tools/mkspiffs/0.2.0 -prefs=runtime.tools.esptool.path=/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/home/jp/.arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
-#command = "/Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
-check_call(command, shell=True) 
+call(command, shell=True)
+
+if os.name == 'Linux':
+    command = "/home/jp/arduino/arduino-builder -compile -logger=machine -hardware /home/jp/arduino/hardware -hardware /home/jp/.arduino15/packages -tools /home/jp/arduino/tools-builder -tools /home/jp/arduino/hardware/tools/avr -tools /home/jp/.arduino15/packages -built-in-libraries /home/jp/arduino/libraries -libraries /home/jp/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/home/jp/.arduino15/packages/esp8266/tools/mkspiffs/0.2.0 -prefs=runtime.tools.esptool.path=/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/home/jp/.arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
+elif os.name == 'Darwin' or 'posix':
+    command = "/Applications/Arduino.app/Contents/Java/arduino-builder -compile -logger=machine -hardware /Applications/Arduino.app/Contents/Java/hardware -hardware /Users/jp/Library/Arduino15/packages -tools /Applications/Arduino.app/Contents/Java/tools-builder -tools /Applications/Arduino.app/Contents/Java/hardware/tools/avr -tools /Users/jp/Library/Arduino15/packages -built-in-libraries /Applications/Arduino.app/Contents/Java/libraries -libraries /Users/jp/Documents/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/mkspiffs/0.1.2 -prefs=runtime.tools.esptool.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/Users/jp/Library/Arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
+else:
+    sys.exit("os " + os.name + " unsupported yet!")
+check_call(command, shell=True)
 
 list = serial.tools.list_ports.comports()
 connected = []
 for element in list:
-   connected.append(element.device)
-   port = element.device
-   #command = "/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9/esptool -vv -cd nodemcu -cb 921600 -cp " + port + " -ca 0x00000 -cf " + build_path + "/synchrobike.ino.bin"
-   command = "/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12/esptool -vv -cd nodemcu -cb 921600 -cp " + port + " -ca 0x00000 -cf " + build_path + "/synchrobike.ino.bin"
-   call(command, shell=True) 
+    connected.append(element.device)
+    port = element.device
+    if os.name == 'Linux':
+        command = "/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12/esptool -vv -cd nodemcu -cb 921600 -cp " + port + " -ca 0x00000 -cf " + build_path + "/synchrobike.ino.bin"
+    elif os.name == 'Darwin' or 'posix':
+        command = "/Users/jp/Library/Arduino15/packages/esp8266/tools/esptool/0.4.9/esptool -vv -cd nodemcu -cb 921600 -cp " + port + " -ca 0x00000 -cf " + build_path + "/synchrobike.ino.bin"
+    else:
+        sys.exit("os " + os.name + " unsupported yet!")
+    call(command, shell=True)
 print("Connected COM ports: " + str(connected))

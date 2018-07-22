c10ff88a5ef4844db1160448adcb58b9c9955ab4
Jason Poindexter <poindexter.json@gmail.com>
2018-07-22 14:16:08 -0700

    Change animatons in sync, color pallets need work

--- synchrobike.ino
+++ synchrobike.ino
@@ -8,7 +8,7 @@ painlessMesh  mesh;
 
 #include <ESP8266TrueRandom.h>
 
-#define FASTLED_ESP8266_DMA // Use ESP8266'a DMA (GPIO3 / Commonly RX)
+//#define FASTLED_ESP8266_DMA // Use ESP8266'a DMA (GPIO3 / Commonly RX)
                             // WS281x LEDs must be unplugged while uploading sketch
 #include <FastLED.h>
 
@@ -17,7 +17,7 @@ painlessMesh  mesh;
 #endif
 
 #define LED_PIN     13 // This pin is ignorred when using FASTLED_ESP8266_DMA
-#define NUM_LEDS    50
+#define NUM_LEDS    75
 #define BRIGHTNESS  64 // 0 - 255
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
@@ -28,11 +28,12 @@ CRGB leds[NUM_LEDS];
 CRGBPalette16 currentPalette(CRGB::Black);
 CRGBPalette16 targetPalette(OceanColors_p);
 TBlendType    currentBlending;                                // NOBLEND or LINEARBLEND 
- 
+
 static int16_t dist;                                          // A random number for our noise generator.
 uint16_t xscale = 30;                                         // Wouldn't recommend changing this on the fly, or the animation will be really blocky.
 uint16_t yscale = 30;                                         // Wouldn't recommend changing this on the fly, or the animation will be really blocky.
 uint8_t maxChanges = 24;                                      // Value for blending between palettes.
+
 boolean force_direction_change = false;
 boolean force_pallet_change = false;
 
@@ -86,55 +87,67 @@ void setup() {
 
 void loop() {
   mesh.update();
-  showAnimations();
+  showAnimation();
 }
 
-int previous_seed = 0;
-uint8_t prev_second_hand = 0;
-uint8_t prev_millisecond_hand = 0;
+uint8_t prev_second = 0;
+uint8_t prev_millis = 0;
 uint8_t prev_direction_time = 0;
 
 bool direction = rand() % 2;
 
-void showAnimations(){
-  int current_time = mesh.getNodeTime();
-
-
-  // Generate the animation ever (ms)
-  int millisecond_hand = (current_time / 10000 % 60); // enter every 10ms
-  if (prev_millisecond_hand != millisecond_hand) {   
-    nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);      // AWESOME palette blending capability.
-//    fillnoise8();
-    confetti(); // Update the LED array with noise at the new location
-    prev_millisecond_hand = millisecond_hand;
-    FastLED.show();
-  }
-
-  // change direction every random (s);
-  int direction_time = current_time / 10000000 / direction_change;
-  if((prev_direction_time != direction_time) || force_direction_change) {
-    randomSeed(direction_time);
-    Serial.printf("SYNCHROBIKE: directionChange has been reached: %u\n", direction_time);
-    direction = random(0,255) % 2;
-    direction_change = random(5,6);
-    prev_direction_time = direction_time;
-    force_direction_change = false;
-  }
-
-  // change color pallet every random (s)
- int second_hand = (current_time / 1000000) / HOLD_PALETTES_X_TIMES_AS_LONG;
-  if ((prev_second_hand != second_hand) || force_pallet_change) {
-    randomSeed(second_hand);
-    Serial.printf("SYNCHROBIKE: change color pallets: %u\n", second_hand);
-    targetPalette = CRGBPalette16(CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 192, random(128,255)), CHSV(random(0,255), 255, random(128,255)));
-    prev_second_hand = second_hand;
-    force_pallet_change = false;
-    changeConfetti();
-  }
+int16_t getMillis(){
+    return (mesh.getNodeTime() / 1000);
 }
 
+int16_t getSecond(){
+    return (mesh.getNodeTime() / 1000000);
+}
+
+int16_t getMinute(){
+    return ((mesh.getNodeTime() / 1000000) / 60);
+}
+
+
+void showAnimation(){
+    changePalette();
+    showLEDs();
+
+}
+
+// Generate the animation ever (ms)
+void showLEDs(){
+    // Generate the animation ever (ms)
+    int millis = getMillis() / 10;
+    if (prev_millis != millis) {
+        switch((getSecond() / 30) % 2) {
+            case 0:
+                nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);   // AWESOME palette blending capability.
+                fillnoise8();
+            case 1:
+                confetti();
+        }
+        prev_millis = millis;
+        FastLED.show();
+    }
+}
+
+void changePalette(){
+    // change color pallet every random (s)
+    int second = getSecond() / HOLD_PALETTES_X_TIMES_AS_LONG;
+    if ((prev_second != second) || force_pallet_change) {
+        Serial.printf("Change color pallet %u\n", second);
+        randomSeed(second);
+        targetPalette = CRGBPalette16(CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 255, random(128,255)), CHSV(random(0,255), 192, random(128,255)), CHSV(random(0,255), 255, random(128,255)));
+        prev_second = second;
+        force_pallet_change = false;
+    }
+}
+
+
+
 /* =============== NOISE ANIMATION =============== */
- 
+
 void fillnoise8() {
   for(int i = 0; i < NUM_LEDS; i++) {                                      // Just ONE loop to fill up the LED array as all of the pixels change.
     uint8_t index = inoise8(0, dist+i* yscale) % 255;               // Get a value from the noise function. I'm using both x and y axis.
@@ -145,6 +158,18 @@ void fillnoise8() {
   } else {
     dist -= beatsin8(10,1,4, mesh.getNodeTime()); 
   }
+}
+
+void changeDirection(){
+    // change direction every random (s);
+    int direction_time = getSecond() / direction_change;
+    if((prev_direction_time != direction_time) || force_direction_change) {
+        randomSeed(direction_time);
+        direction = random(0,255) % 2;
+        direction_change = random(5,6);
+        prev_direction_time = direction_time;
+        force_direction_change = false;
+    }
 }
 
 /* =============== CONFETTI ANIMATION =============== */
@@ -159,44 +184,18 @@ uint8_t thisdelay = 5;
 
 void confetti() {                                             // random colored speckles that blink in and fade smoothly
     fadeToBlackBy(leds, NUM_LEDS, thisfade);                    // Low values = slower fade.
-//    random16_set_seed((mesh.getNodeTime() / 1000));
     int pos = random16(NUM_LEDS);                               // Pick an LED at random.
-    leds[pos] += CHSV((thishue + random16(huediff))/4 , thissat, thisbri);  // I use 12 bits for hue so that the hue increment isn't too quick.
-    thishue = thishue + thisinc;                                // It increments here.
+    leds[pos] = ColorFromPalette(currentPalette, thishue + random16(huediff)/4 , thisbri, currentBlending);
+    thishue = thishue + thisinc;                                   // It increments here.
 }
 
 
-void changeConfetti() {                                             // A time (rather than loop) based demo sequencer. This gives us full control over the length of each sequence.
-//    uint8_t secondHand = (mesh.getNodeTime() / 1000000) %
-//                         15;                // IMPORTANT!!! Change '15' to a different value to change duration of the loop.
-//    static uint8_t lastSecond = 99;                             // Static variable, means it's only defined once. This is our 'debounce' variable.
-//    if (lastSecond != secondHand) {                             // Debounce to make sure we're not repeating an assignment.
-//        lastSecond = secondHand;
-//        switch (secondHand) {
-//            case 0:
-                thisinc = 1;
-                thishue = 192;
-                thissat = 255;
-                thisfade = 18;
-                huediff = 256;
-//                break;  // You can change values here, one at a time , or altogether.
-//            case 5:
-//                thisinc = 2;
-//                thishue = 128;
-//                thisfade = 8;
-//                huediff = 64;
-//                break;
-//            case 10:
-//                random16_set_seed((mesh.getNodeTime() / 1000000) / HOLD_PALETTES_X_TIMES_AS_LONG);
-//                thisinc = 1;
-//                thishue = random(255);
-//                thisfade = 1;
-//                huediff = 16;
-//                break;      // Only gets called once, and not continuously for the next several seconds. Therefore, no rainbows.
-//            case 15:
-//                break;                                                                // Here's the matching 15 for the other one.
-//        }
-//    }
-}
+//void changeConfetti() {                                             // A time (rather than loop) based demo sequencer. This gives us full control over the length of each sequence.
+//    thisinc = 1;
+//    thishue = 192;
+//    thissat = 255;
+//    thisfade = 18;
+//    huediff = 256;
+//}
 
 

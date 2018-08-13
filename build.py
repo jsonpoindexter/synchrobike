29c5e30644742ebd737277eceb43dd5b027d8d45
Jason Poindexter <jason@launchbadge.com>
2018-08-13 13:03:28 -0700

    Create build.py for platformio. Untested on Ubuntu

---
+++ build.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import serial.tools.list_ports
+import os, sys, platform, subprocess, time
+from pathlib import Path
+
+platform = platform.system()
+home = str(Path.home())
+
+# # Test Build sketch
+# if platform == 'Linux':
+#     command = "/home/jp/arduino/arduino-builder -dump-prefs -logger=machine -hardware /home/jp/arduino/hardware -hardware /home/jp/.arduino15/packages -tools /home/jp/arduino/tools-builder -tools /home/jp/arduino/hardware/tools/avr -tools /home/jp/.arduino15/packages -built-in-libraries /home/jp/arduino/libraries -libraries /home/jp/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/home/jp/.arduino15/packages/esp8266/tools/mkspiffs/0.2.0 -prefs=runtime.tools.esptool.path=/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/home/jp/.arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
+# elif platform == 'Darwin' or 'posix':
+#     home = os.path.expanduser("~")
+#     command = home + "/.platformio/penv/bin/platformio"
+# else:
+#     sys.exit("os " + platform + " unsupported yet!")
+# call(command, shell=True)
+
+# Build Sketch
+# if platform == 'Linux':
+#     command = "/home/jp/arduino/arduino-builder -dump-prefs -logger=machine -hardware /home/jp/arduino/hardware -hardware /home/jp/.arduino15/packages -tools /home/jp/arduino/tools-builder -tools /home/jp/arduino/hardware/tools/avr -tools /home/jp/.arduino15/packages -built-in-libraries /home/jp/arduino/libraries -libraries /home/jp/Arduino/libraries -fqbn=esp8266:esp8266:d1_mini:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M -ide-version=10805 -build-path " + build_path + " -warnings=none -build-cache " + build_path + " -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.mkspiffs.path=/home/jp/.arduino15/packages/esp8266/tools/mkspiffs/0.2.0 -prefs=runtime.tools.esptool.path=/home/jp/.arduino15/packages/esp8266/tools/esptool/0.4.12 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=/home/jp/.arduino15/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2 -verbose " + dir_path + "/synchrobike.ino"
+# elif platform == 'Darwin' or 'posix':
+#     home = os.path.expanduser("~")
+#     command = home + "/.platformio/penv/bin/platformio run --target upload --upload-port "
+# else:
+#     sys.exit("os " + platform + " unsupported yet!")
+# call(command, shell=True)
+
+
+# Write to devices
+
+serial_devices = serial.tools.list_ports.comports()
+connected = []
+for serial_device in serial_devices:
+    port = serial_device.device
+    # if port !=  "/dev/cu.Bluetooth-Incoming-Port":
+    if platform == 'Linux' or 'Darwin' or 'posix':
+        command = home + "/.platformio/penv/bin/platformio run --target upload --upload-port " + port
+        subprocess.call(command, shell=True)
+        connected.append(serial_device.device)
+    else:
+        sys.exit("os " + platform + " unsupported yet!")
+        
+print("Connected COM ports: " + str(connected))
